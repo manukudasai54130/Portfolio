747f99e226b28f7e0355366bf4b717e4fbf4b288
Pierre-Emmanuel <pierre.e.groux@gmail.com>
2024-02-07 14:07:09 +0100

    Fichiers javascript pour la maquette du site weirdmoon tattoo

---
+++ JS/slider-on-scroll.js
@@ -0,0 +1,144 @@
+
+
+TEST fin body
+
+
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+    crossorigin="anonymous"></script>
+
+"owl-stage-outer 603.667×340
+
+1883×364.5
+    583.667×204.5
+
+    <section class="testimony">
+        <div class="container-5">
+            <h1 class="text-center white">Témoignages</h1>
+            <div class="row">
+                <div class="col-md-4 text-center">
+                    <div class="profile">
+                        <img src="images/vcard 1.jpg" class="testimony-photo">
+                        <blockquote>She's soo damm good</blockquote>
+                        <h3> Cat from hell<span> griffé 8 fois</span></h3>
+                    </div>
+                </div>
+                <div class="col-md-4 text-center">
+                    <div class="profile">
+                        <img src="images/vcard 2.jpg" class="testimony-photo">
+                        <blockquote>ohhhh j'en ai chié mais le résultat est là</blockquote>
+                        <h3> Bébou  cat <span> 1ère griffure</span></h3>
+                    </div>
+                </div>
+                <div class="col-md-4 text-center">
+                    <div class="profile">
+                        <img src="images/vcard 3.jpg" class="testimony-photo">
+                        <blockquote>Elle offre les bonbons et les clopes</blockquote>
+                        <h3> Cocaino Cat <span> griffé 900 fois</span></h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </section>
+
+
+
+<section class="testimony">
+    <div class="conatainer-5">
+        <h1>Témoignages</h1>
+        <div class="row">
+            <div class="col-md-4 text-center">
+                <div class="owl-carousel owl-theme">
+                    <div class="item">
+                        <img src="images/vcard 1.jpg" class="testimony-photo">
+                        <blockquote>She's soo damm good</blockquote>
+                        <h3> Cat from hell<span> griffé 8 fois</span></h3>
+                    </div>
+                    <div class="item"><h4>2</h4></div>
+                    <div class="item"><h4>3</h4></div>
+                    <div class="item"><h4>4</h4></div>
+                    <div class="item"><h4>5</h4></div>
+                    <div class="item"><h4>6</h4></div>
+                    <div class="item"><h4>7</h4></div>
+                    <div class="item"><h4>8</h4></div>
+                    <div class="item"><h4>9</h4></div>
+                    <div class="item"><h4>10</h4></div>
+                    <div class="item"><h4>11</h4></div>
+                    <div class="item"><h4>12</h4></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="actu">
+    <div class="container-6">
+        <div class="row">
+            <div class="owl-carousel owl-theme">
+                <div class="item"> 
+                    <img src="images/actu 1.jpg" alt="">            
+                </div>
+                <div class="item"> 
+                    <img src="images/actu 1.jpg" alt="">                 
+                </div>
+                <div class="item"> 
+                    <img src="images/actu 1.jpg" alt="">              
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+Base img 348 489
+    <div class="container-6">
+        <div class="row">
+            <div class="col"> 
+                <img src="images/actu 1.jpg" alt="">            
+            </div>
+            <div class="col"> 
+                <img src="images/actu 1.jpg" alt="">                 
+            </div>
+            <div class="col"> 
+                <img src="images/actu 1.jpg" alt="">              
+            </div>
+        </div>
+    </div>
+
+    <div class="boutton">
+     <button type="button" class="btn btn-outline-dark">Tout voir</button>
+    </div>
+
+
+    .container-6{
+    display: flex;
+    justify-content: center;
+}
+
+.container-6 .row{
+    margin-left: 10px;
+    margin-right: 10px;
+
+}
+
+.container-6 .col{
+    display: flex;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    margin-right: 0px;
+    width: 348px;
+    height: 489px;
+    
+   
+}
+
+.container-6 .col:hover{
+    box-shadow: 0 0 15px 5px rgba(0,0,0,0.2);
+    cursor: pointer;
+    transition: 0.5s;
+    
+}
